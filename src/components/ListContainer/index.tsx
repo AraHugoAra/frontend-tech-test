e1cdd5007b567704e481b71269e3d4994d33ef32
Hugo Aragones <hugo@qlower.com>
2023-10-29 11:41:26 +0100

    Refactored loader into its own component

--- src/components/ListContainer/index.tsx
+++ src/components/ListContainer/index.tsx
@@ -3,18 +3,24 @@ import useFetch from "../../hooks/useFetch";
 import calculatePageNumber from "../../api/calculatePageNumber";
 import CharacterCard from "../CharacterCard";
 import Pagination from "../Pagination";
-import { SearchBarContext, SearchBarContextStateType } from "../../context/searchBarContext";
-import { CharacterType } from '../../api/types'
+import {
+  SearchBarContext,
+  SearchBarContextStateType,
+} from "../../context/searchBarContext";
+import { CharacterType } from "../../api/types";
+import Loader from "../Loader";
 
 const ListContainer = () => {
-  const { searchText }: SearchBarContextStateType = useContext(SearchBarContext)
+  const { searchText }: SearchBarContextStateType =
+    useContext(SearchBarContext);
 
-  // USE OF THE API
   const [currentPage, setCurrentPage] = useState<number>(1);
   const resultsPerPage = 4;
   const offset = (currentPage - 1) * resultsPerPage;
   const { data, loading, error } = useFetch(
-    `/characters?${searchText ? `nameStartsWith=${searchText}&` : ''}limit=${resultsPerPage}&offset=${offset > 0 ? offset : 0}`,
+    `/characters?${
+      searchText ? `nameStartsWith=${searchText}&` : ""
+    }limit=${resultsPerPage}&offset=${offset > 0 ? offset : 0}`,
     {},
     [currentPage, searchText]
   );
@@ -25,31 +31,19 @@ const ListContainer = () => {
     resultsPerPage
   );
 
-    useEffect(() => {
-      //Resets current page after every new name query
-      setCurrentPage(1)
-    }, [searchText])
-  // const data = placeholder;
-  // const loading = false;
-  // const error = null;
-
-  // const [currentPage, setCurrentPage] = useState(1);
-
-  // const resultsPerPage = 4;
-  // const lastIndex = currentPage * resultsPerPage;
-  // const firstIndex = lastIndex - resultsPerPage;
-  // const pagesNumber = calculatePageNumber(
-  //   data.response.data.data.results.length,
-  //   resultsPerPage
-  // );
+  useEffect(() => {
+    //Resets current page after every new name query
+    setCurrentPage(1);
+  }, [searchText]);
 
   return !loading && !error ? ( //TO DO: "No results" screen
     <div className="list-page">
       <div className="list-container">
-        {data?.response?.data?.data.results
-          .map((item: CharacterType, index: number) => (
+        {data?.response?.data?.data.results.map(
+          (item: CharacterType, index: number) => (
             <CharacterCard key={item.id ?? index} character={item} /> //id is optionnal in database so index has been added to prevent key's absence
-          ))}
+          )
+        )}
         {pagesNumber > 1 && (
           <Pagination
             firstIndex={firstIndex}
@@ -64,7 +58,7 @@ const ListContainer = () => {
     </div>
   ) : (
     <div className="list-container">
-      <span className="loader"></span>
+      <Loader />
     </div>
   );
 };
